bcf7eed3e5192f0aab5561ccebbb48f7b336b8e0
zafar-automat <mz.khan1929@gmail.com>
2021-06-28 19:26:20 -0400

    no need to put require "selenium-webdriver" in steps file

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,6 +9,20 @@ else
 end
 
 
+# require 'rspec'
+# require 'page-object'
+# require 'data_magic'
+# require 'yaml'
+#
+#
+# if ENV['browser'] == nil
+#   $browser_type = :chrome
+# else
+#   $browser_type = ENV['browser']
+# end
+
+
+
 if ENV['env'] == nil
   environment_name = ''
 else
--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -8,7 +8,7 @@ require 'watir'
 
 Before do |scenario|
   DataMagic.load_for_scenario(scenario)
-  @browser = Watir::Browser.new :chrome
+  @browser = Watir::Browser.new $browser_type
   $browser = @browser
   $browser.driver.manage.window.maximize
 end
